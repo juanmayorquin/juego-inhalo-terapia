bf0917d08c9c30b0895dd27af2c619f86bea922d
Juan José Mayorquín <juanjmayor@outlook.es>
2023-10-25 13:16:42 -0500

    Se modifica la forma en las que se muestran las preguntas

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,26 +3,49 @@ import { IconArrowRight } from '@tabler/icons-react';
 import { questions } from "../data/questions";
 import { useSwiper } from "swiper/react";
 
+import { useState } from "react";
+
 export function Question({ pregunta }) {
   const swiper = useSwiper();
+  const [respuestas, setRespuestas] = useState([]);
+
+  const handleRespuestaSeleccionada = (isCorrect) => {
+    setRespuestas([...respuestas, isCorrect]);
+  };
+
+  const mostrarResultados = () => {
+    // Calcular los resultados y mostrarlos en una nueva pantalla
+  };
+
   return (
     <section className="relative h-screen mx-auto flex flex-col items-center justify-center snap-start">
+      <h2 className="text-white text-xl text-center pb-10">{pregunta.question}</h2>
       <div className="flex flex-col md:flex-row justify-center items-center gap-5 h-2/3 md:max-w-[90%]">
         {pregunta.choices.map((choice, index) => (
-          <Card key={index} imageSrc={choice.img} text={choice.text} isCorrect={choice.isCorrect} />
+          <Card
+            key={index}
+            imageSrc={choice.img}
+            text={choice.text}
+            isCorrect={choice.isCorrect}
+            onRespuestaSeleccionada={handleRespuestaSeleccionada}
+          />
         ))}
-        {
-          pregunta !== questions[questions.length - 1] ?
-            <a onClick={() => swiper.slideNext()} className="bg-lime-400 flex items-center justify-center w-[90%] p-3 rounded-md md:absolute md:w-auto md:bottom-10 md:right-10 transition-all hover:cursor-pointer hover:scale-105 hover:bg-lime-300 opacity-100">
-              <IconArrowRight className="inline-block w-full ml-0" size={30}/>
-            </a>
-            :
-            <a className="bg-lime-400 flex items-center justify-center w-[90%] p-3 rounded-md md:absolute md:w-auto md:bottom-10 md:right-10 font-medium transition-all hover:cursor-pointer hover:scale-105 hover:bg-lime-300">
-              Finalizar
-            </a>
-        }
+        {pregunta !== questions[questions.length - 1] ? (
+          <a
+            onClick={() => swiper.slideNext()}
+            className="bg-lime-400 flex items-center justify-center w-[90%] p-3 rounded-md md:absolute md:w-auto md:bottom-10 md:right-10 transition-all hover:cursor-pointer hover:scale-105 hover:bg-lime-300 opacity-100"
+          >
+            <IconArrowRight className="inline-block w-full ml-0" size={30} />
+          </a>
+        ) : (
+          <a
+            onClick={mostrarResultados}
+            className="bg-lime-400 flex items-center justify-center w-[90%] p-3 rounded-md md:absolute md:w-auto md:bottom-10 md:right-10 font-medium transition-all hover:cursor-pointer hover:scale-105 hover:bg-lime-300"
+          >
+            Finalizar
+          </a>
+        )}
       </div>
-
     </section>
   );
 }

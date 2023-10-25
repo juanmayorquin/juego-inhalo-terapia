f9e85e750dd3e8b910584285d73224a63f964b83
Juan José Mayorquín <juanjmayor@outlook.es>
2023-10-25 13:55:49 -0500

    Correcciones menores en estilos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,12 +7,12 @@ export function Card ({ imageSrc, text, isCorrect }) {
         if (isCorrect) {
             setBgColor('bg-green-300 scale-[1.025] animate-pulse animate-once animate-duration-150');
         } else {
-            setBgColor('bg-red-300 animate-shake animate-duration-150');
+            setBgColor('bg-red-300 scale-[0.975] animate-shake animate-duration-150');
         }
     };
 
     return (
-        <div className={`rounded-lg p-8 flex flex-col items-center justify-center w-[90%] min-h-[16rem] md:min-h-[30rem] drop-shadow-2xl md:hover:scale-[1.015] transition-all ${bgColor}`} onClick={checkIfCorrect}>
+        <div className={`rounded-lg p-8 flex flex-col items-center justify-center w-[85%] min-h-[12rem] md:min-h-[30rem] drop-shadow-2xl md:hover:scale-[1.015] transition-all ${bgColor}`} onClick={checkIfCorrect}>
             <img src={imageSrc} alt="card image" className="w-1/2" />
             <p className="text-center text-lg mt-4">{text}</p>
         </div>
